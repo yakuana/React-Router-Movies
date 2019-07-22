efe1d0b01a362a83a1eaa9c0193ba246c2a3c1f8
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-07-22 14:01:29 -0500

    WIP: Part 1 complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
 
+// import Route 
+import { Route } from 'react-router-dom';
+
 import SavedList from './Movies/SavedList';
+
+// import Movie and MovieList 
+import MovieList from "./Movies/MovieList"; 
+import Movie from "./Movies/Movie"; 
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
+
+  console.log(savedList); 
 
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
@@ -12,7 +22,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route path="/" exact component={MovieList}/>
+      <Route path="/movies:id" component={Movie}/>
     </div>
   );
 };
